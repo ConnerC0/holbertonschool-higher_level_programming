5dd514ca0fe522ee8464a7e293a7628c2825ce50
root <4908@holbertonschool.com>
2022-08-29 21:55:45 -0700

    fix

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+lastdigit = number  % 10
 if number > 5:
-    print("Last digit of" + number + "is" + number % 10 + "and is greater than 5")
+    print("Last digit of {} is {} and is greater than 5" .format(number, lastdigit))
 elif number < 6:
-    print("Last digit of" + number + "is" + number % 10 + "and is less than 6 and not 0")
+    print("Last digit of {} is {} and is less than 6 and not 0" .format(number, lastdigit))
 elif number == 0:
-    print("Last digit of" + number + "is" + number % 10 + "and is 0")
+    print("Last digit of {} is {} and is 0" .format(number, lastdigit))
