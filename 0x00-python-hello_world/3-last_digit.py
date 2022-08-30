ee41c3080da84000fe525f3f9b372d4594e5a85b
root <4908@holbertonschool.com>
2022-08-29 21:44:53 -0700

    fix

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -5,5 +5,5 @@ if number > 5:
     print("Last digit of" + number + "is" + number[-1:] + "and is greater than 5")
 elif number < 6:
     print("Last digit of" + number + "is" + number[-1:] + "and is less than 6 and not 0")
-else number == 0:
+elif number == 0:
     print("Last digit of" + number + "is" + number[-1:] + "and is 0")
