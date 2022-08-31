7054e1aeb90de5373259f0a02f7be19ad1c09c84
root <4908@holbertonschool.com>
2022-08-31 12:45:12 -0700

    bugfix

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -4,6 +4,7 @@ __name__ == "__main__"
 if len(argv) == 1:
     print("0 arguments.")
 else:
-    print("{} argments:".format(len(argv) - 1))
+    s = "arguments"
+    print("{} {}:".format(len(argv) - 1, s[:-1] if len(argv) == 2 else s))
     for x in range(1, len(argv)):
         print("{}: {}".format(x, argv[x]))
