391708b6497be94d7923bf071d2cabf5004acd07
root <4908@holbertonschool.com>
2022-09-01 09:03:40 -0700

    fix

--- 0x02-python-data_structures/0-print_list_integer.py
+++ 0x02-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    string = ['{}'.format(x) for x in my_list]
     for x in range(len(my_list)):
-            print(my_list[x])
+            print("{}".format(my_list[x]))
