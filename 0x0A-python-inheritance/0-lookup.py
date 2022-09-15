5a745d471f53b54fed3f31c03f9636c6c25e8377
root <4908@holbertonschool.com>
2022-09-15 16:53:33 -0700

    forgot to give execute permissions

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-""" task 0 module """
-
-
 def lookup(obj):
-    """
-    function that returns the list of available 
-    attributes and methods of an object
-    """
-    return list(dir(obj))
+    return dir(obj)
