cd3e12bdc8aaeba5139eb9c1b8123715277c68e4
root <4908@holbertonschool.com>
2022-09-15 17:20:31 -0700

    doc update

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" Task 2 is same class module """
+
+
 def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of
+    the specified class ; otherwise False
+    """
     if type(obj) == a_class:
         return True
     return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" task 4 inherits from module """
+
+
 def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False
+    """
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
