4e1ab6c2d0b613723da5af7b519b5c6cf942d9af
root <4908@holbertonschool.com>
2022-09-19 14:00:58 -0700

    task 3 and 4

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" task 3 to JSON string """
+
+
+def to_json_string(my_obj):
+    """ a methond that returns the JSON representation of an object """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" task 4 from json string """
+
+
+def from_json_string(my_str):
+    """ return an object represented by a JSON string """
+    return json.loads(my_obj)
