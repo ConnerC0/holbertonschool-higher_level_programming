776b178378adc470ba39247f8c56826269e50935
root <4908@holbertonschool.com>
2022-10-20 14:15:16 -0700

    fix

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,13 +6,13 @@ FROM states WHERE name = '" as an input
 
 
 import MySQLdb
-from sys improt argv
+from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                           passwd=argv[2], db=argv[3], charset="utf-8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name like %s ORDER BY ID ASC", (argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY ID ASC", (argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
