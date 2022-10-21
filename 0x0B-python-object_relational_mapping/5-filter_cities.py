193863bebbedc9b9094167bb9ef86f11126fae90
root <4908@holbertonschool.com>
2022-10-21 08:51:44 -0700

    stuff

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that takes in the name of a state as an argument and lists all cities 
+a script that takes in the name of a state as an argument and lists all cities
 of that state, using the database hbtn_0e_4_usa
 """
 
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT cities.name FROM cities \
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
