f6539b24f73362913aa63c0eb7fa022482342386
root <4908@holbertonschool.com>
2022-10-21 08:54:09 -0700

    stuff

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,10 +10,10 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                       argv[2],
-                                                                       argv[3]))
+                                                                    argv[2],
+                                                                    argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
