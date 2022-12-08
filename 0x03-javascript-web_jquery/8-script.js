22330b1bf86c5c410c5eb130acb2dce227119dd0
ConnerC0 <conner.curtis72@gmail.com>
2022-12-07 16:55:14 -0800

    completed

---
+++ 0x03-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', (content) => {
+  const movies = content.results;
+  for (const movie in movies) {
+    $('#list_movies').append('<li>' + movies[movie].title + '</li>');
+  }
+});
